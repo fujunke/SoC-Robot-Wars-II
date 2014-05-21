ceab409e6a7e5d2bf015125c3b1b8381a9622479
Apoclyps <kyle90adam@hotmail.com>
2014-05-22 00:16:50 +0100

    Robotic Arm set up to use Java.
    Jar added: Resources for installation available at :
    
    http://libusbjava.sourceforge.net/wp/?page_id=8
    
    http://sourceforge.net/apps/trac/libusb-win32/wiki
    
    http://www.libusb.org/
    
    http://gampageek.blogspot.co.uk/2013/01/as-well-as-swanning-around-internet.html
    
    http://fivedots.coe.psu.ac.th/~ad/jg/nui06/index.html

---
+++ Robot-Client/Raspberry Pi Code/RoboticArm/src/main/java/KyleHarrison/RoboticArm/App.java
@@ -0,0 +1,13 @@
+package KyleHarrison.RoboticArm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
